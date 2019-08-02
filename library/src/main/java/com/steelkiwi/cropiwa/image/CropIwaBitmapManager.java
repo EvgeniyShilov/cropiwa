7d8f76d5f0e53c86359621af8353ca9b548e60e1
Evgeniy Shilov <eugshilov@gmail.com>
2019-08-02 16:38:12 +0300

    migrated to androidx fixed dependencies and gradle versions

--- library/src/main/java/com/steelkiwi/cropiwa/image/CropIwaBitmapManager.java
+++ library/src/main/java/com/steelkiwi/cropiwa/image/CropIwaBitmapManager.java
@@ -6,8 +6,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.steelkiwi.cropiwa.config.CropIwaSaveConfig;
 import com.steelkiwi.cropiwa.shape.CropIwaShapeMask;
@@ -26,7 +27,7 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.steelkiwi.cropiwa.util.CropIwaUtils.*;
+import static com.steelkiwi.cropiwa.util.CropIwaUtils.closeSilently;
 
 /**
  * @author Yaroslav Polyakov https://github.com/polyak01
